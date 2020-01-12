813baea17eaa701d8b29002395bcf7de83843aaa
Kevin G Lam <kevlam10@outlook.com>
2020-01-12 12:00:58 -0500

    added top nav animation

--- js/script.js
+++ js/script.js
@@ -13,4 +13,40 @@ $(document).ready(setup);
 
 function setup() {
 
+
+    $("#about").hover(function(){
+  $(this).css("color", "#FDBD06");
+  }, function(){
+  $(this).css("color", "white");
+});
+
+$("#work").hover(function(){
+$(this).css("color", "#FDBD06");
+}, function(){
+$(this).css("color", "white");
+});
+
+$("#cv").hover(function(){
+$(this).css("color", "#FDBD06");
+}, function(){
+$(this).css("color", "white");
+});
+
+$("#about2").hover(function(){
+$(this).css("color", "#FDBD06");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#work2").hover(function(){
+$(this).css("color", "#FDBD06");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#cv2").hover(function(){
+$(this).css("color", "#FDBD06");
+}, function(){
+$(this).css("color", "black");
+});
 }
