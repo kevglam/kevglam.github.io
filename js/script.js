8f446fd7dcb104e7cb6e31faefa60042138278b7
Kevin G Lam <kevlam10@outlook.com>
2020-01-18 12:03:57 -0500

    Purple top nav hover added

--- js/script.js
+++ js/script.js
@@ -15,37 +15,37 @@ function setup() {
 
 
     $("#about").hover(function(){
-  $(this).css("color", "#FDBD06");
+  $(this).css("color", "#810FF5");
   }, function(){
   $(this).css("color", "white");
 });
 
 $("#work").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#cv").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#about2").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#work2").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#cv2").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "black");
 });
