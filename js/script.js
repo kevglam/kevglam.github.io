d084ca9ffa73f7136530b852cd6d418445f51b44
Kevin G Lam <kevlam10@outlook.com>
2021-01-04 16:05:22 -0500

    More branding changes
    Changed top nav font to Rubik Mono One + Removed secondary purple color from the website.

--- js/script.js
+++ js/script.js
@@ -15,37 +15,37 @@ function setup() {
 
 
     $("#about").hover(function(){
-  $(this).css("color", "#810FF5");
+  $(this).css("color", "#FDBD06");
   }, function(){
   $(this).css("color", "white");
 });
 
 $("#work").hover(function(){
-$(this).css("color", "#810FF5");
+$(this).css("color", "#FDBD06");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#cv").hover(function(){
-$(this).css("color", "#810FF5");
+$(this).css("color", "#FDBD06");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#about2").hover(function(){
-$(this).css("color", "#810FF5");
+$(this).css("color", "#FDBD06");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#work2").hover(function(){
-$(this).css("color", "#810FF5");
+$(this).css("color", "#FDBD06");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#cv2").hover(function(){
-$(this).css("color", "#810FF5");
+$(this).css("color", "#FDBD06");
 }, function(){
 $(this).css("color", "black");
 });
