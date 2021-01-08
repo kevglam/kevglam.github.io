bdbb31fd9c41401aafaf745fb61fd83bac4736b9
Kevin G Lam <kevlam10@outlook.com>
2021-01-08 14:51:47 -0500

    Updated branding colors of the whole site
    Changed accent colors to purple

--- js/script.js
+++ js/script.js
@@ -15,73 +15,73 @@ function setup() {
 
 
     $("#about").hover(function(){
-  $(this).css("color", "#FDBD06");
+  $(this).css("color", "#810FF5");
   }, function(){
   $(this).css("color", "white");
 });
 
 $("#work").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#cv").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#about2").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#work2").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#cv2").hover(function(){
-$(this).css("color", "#FDBD06");
+$(this).css("color", "#810FF5");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#kalifront").hover(function(){
-$(this).attr("src", "assets/images/front/kalifront_ys.jpg");
+$(this).attr("src", "assets/images/clickme_y.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/kalifront.jpg");
 });
 
 $("#helenfront").hover(function(){
-$(this).attr("src", "assets/images/front/heritagefront2_ys.jpg");
+$(this).attr("src", "assets/images/clickme_y.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/heritagefront2.jpg");
 });
 
 $("#mountfront").hover(function(){
-$(this).attr("src", "assets/images/front/interludefront_y.jpg");
+$(this).attr("src", "assets/images/clickme_y.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/interludefront.jpg");
 });
 
 $("#christyfront").hover(function(){
-$(this).attr("src", "assets/images/front/heritagefront_ys.jpg");
+$(this).attr("src", "assets/images/clickme_y.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/heritagefront.jpg");
 });
 
 $("#hubfront").hover(function(){
-$(this).attr("src", "assets/images/front/paletticafront_y.jpg");
+$(this).attr("src", "assets/images/clickme_y.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/paletticafront2.jpg");
 });
 
 $("#dcartfront").hover(function(){
-$(this).attr("src", "assets/images/front/dcartfront_ys.jpg");
+$(this).attr("src", "assets/images/clickme_y.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/dcartfront.jpg");
 });
