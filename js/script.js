44007d07cd7c17cc66e6c5c0b4db70491aae9c2d
kevglam <kevlam10@outlook.com>
2025-10-25 14:55:56 -0400

    BIG PATCH - 25/10/25
    Adding 4 new projects.

--- js/script.js
+++ js/script.js
@@ -15,117 +15,129 @@ function setup() {
 
 
     $("#about").hover(function(){
-  $(this).css("color", "#14d774");
+  $(this).css("color", "#002aff");
   }, function(){
   $(this).css("color", "white");
 });
 
 $("#work").hover(function(){
-$(this).css("color", "#14d774");
+$(this).css("color", "#002aff");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#cv").hover(function(){
-$(this).css("color", "#14d774");
+$(this).css("color", "#002aff");
 }, function(){
 $(this).css("color", "white");
 });
 
 $("#about2").hover(function(){
-$(this).css("color", "#14d774");
+$(this).css("color", "#002aff");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#work2").hover(function(){
-$(this).css("color", "#14d774");
+$(this).css("color", "#002aff");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#cv2").hover(function(){
-$(this).css("color", "#14d774");
+$(this).css("color", "#002aff");
 }, function(){
 $(this).css("color", "black");
 });
 
 $("#kalifront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/kalifront.jpg");
 });
 
 $("#kalifront2").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/ryubag.jpg");
 });
 
 $("#helenfront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/jomofront.jpg");
 });
 
 $("#mountfront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/audiblefront.jpg");
 });
 
 $("#christyfront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
-$(this).attr("src", "assets/images/front/chimerafront.jpg");
+$(this).attr("src", "assets/images/front/heroicfront.jpg");
 });
 
 $("#hubfront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/ryusumfront.jpg");
 });
 
 $("#riotfront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/riotfront.jpg");
 });
 
+$("#pmcffront").hover(function(){
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
+}, function(){
+$(this).attr("src", "assets/images/pmcf/walkheader.jpg");
+});
+
+$("#samfront").hover(function(){
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
+}, function(){
+$(this).attr("src", "assets/images/silver/front.jpg");
+});
+
 $("#hubfront2").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/ryucolfront.jpg");
 });
 
 $("#dcartfront").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/marvelfront.jpg");
 });
 
 $("#dcartfront2").hover(function(){
-$(this).attr("src", "assets/images/front/jadeclick2.jpg");
+$(this).attr("src", "assets/images/front/napalmclick.jpg");
 }, function(){
 $(this).attr("src", "assets/images/front/ryuvaporfront.jpg");
 });
 
 $("#downloadcv").hover(function(){
-$(this).attr("src", "assets/images/jadedownloadcv2.svg");
+$(this).attr("src", "assets/images/napalmdownloadcv02.svg");
 }, function(){
-$(this).attr("src", "assets/images/jadedownloadcv.svg");
+$(this).attr("src", "assets/images/napalmdownloadcv.svg");
 });
 
 $("#seemoreani").hover(function(){
-$(this).attr("src", "assets/images/jadeseemore2.svg");
+$(this).attr("src", "assets/images/napalmseemore02.svg");
 }, function(){
-$(this).attr("src", "assets/images/jadeseemore.svg");
+$(this).attr("src", "assets/images/napalmseemore.svg");
 });
 
 $("#behan").hover(function(){
-$(this).attr("src", "assets/images/jadebehance2.svg");
+$(this).attr("src", "assets/images/napalmbehance02.svg");
 }, function(){
-$(this).attr("src", "assets/images/jadebehance.svg");
+$(this).attr("src", "assets/images/napalmbehance.svg");
 });
 
 
